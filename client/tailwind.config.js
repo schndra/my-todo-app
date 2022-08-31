c7983bb3b783cd95cd9a83e4f6c497849fa374e6
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-08-31 19:54:58 +0530

    added gray colors from tailwind llibrary

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -10,18 +12,25 @@ module.exports = {
     },
 
     colors: {
+      main: "#FFFFFF",
       primary: "#ff6e6c",
       secondary: "#67568c",
       tertiary: "#fbdd74",
       background: "#f4effc",
       buttonTxt: "#1f1235",
 
+      stroke: "#1f1235",
+
       headline: "#1f1135",
       subHeadline: "#1b1325",
       cardBg: "#e2daeb",
       cardhead: "#1f1135",
       cardPara: "#1b1325",
+
+      // gray
+      gray: colors.gray,
     },
+
     extend: {
       fontFamily: {
         body: ["Roboto Mono", "monospace"],
