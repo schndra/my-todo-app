683decf20ce4e76627bce61414cf4bef8adb682d
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-09-15 21:45:24 +0530

    added animations using tailwindcss

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,13 +5,22 @@ const Navbar = () => {
   return (
     <>
       <div className="font-head text-subHeadline capitalize flex items-center justify-center gap-16 bg-secondary h-12 ">
-        <Link to="/" className=" hover:text-gray-300   ">
+        <Link
+          to="/"
+          className=" hover:text-gray-300 hover:scale-125 hover:transition-all  "
+        >
           todo(0)
         </Link>
-        <Link to="/completed" className=" hover:hover:text-gray-300  ">
+        <Link
+          to="/completed"
+          className=" hover:hover:text-gray-300  hover:scale-125 hover:transition-all "
+        >
           complted(0)
         </Link>
-        <Link to="/all" className="hover:text-gray-300 ">
+        <Link
+          to="/all"
+          className="hover:text-gray-300 hover:scale-125 hover:transition-all "
+        >
           All(0)
         </Link>
       </div>
--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { FaTrash } from "react-icons/fa";
-import { FaRegEdit } from "react-icons/fa";
+import { FaCheckSquare } from "react-icons/fa";
 
 const TodoItem = (props) => {
   const { id, title } = props;
+
   return (
     <>
       <li className="list-none flex items-center mt-2 px-2">
         <button type="button" className="mr-2">
-          <FaRegEdit className="text-primary" />
+          {/* if completed true make the check box text-primary */}
+          <FaCheckSquare className="text-gray-400 hover:animate-pulse hover:scale-125" />
         </button>
         <div className="flex w-vw justify-between ">
           <p className="text-cardPara">{title}</p>
           <button type="button">
-            <FaTrash className="text-primary" />
+            <FaTrash className="text-primary hover:animate-bounce" />
           </button>
         </div>
       </li>
