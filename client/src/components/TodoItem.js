f22a8b2655a5ac300c42d67b64682ae62b8e1b32
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-09-15 21:59:28 +0530

    added strike through and checkbox feature

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { FaTrash } from "react-icons/fa";
 import { FaCheckSquare } from "react-icons/fa";
+import { useOutletContext } from "react-router-dom";
 
 const TodoItem = (props) => {
-  const { id, title } = props;
+  const { id, title, completed } = props;
+  const {} = useOutletContext();
 
   return (
     <>
       <li className="list-none flex items-center mt-2 px-2">
         <button type="button" className="mr-2">
           {/* if completed true make the check box text-primary */}
-          <FaCheckSquare className="text-gray-400 hover:animate-pulse hover:scale-125" />
+          <FaCheckSquare
+            className={`${
+              !completed
+                ? `text-gray-400 hover:animate-pulse hover:scale-125`
+                : `text-primary hover:animate-pulse hover:scale-125`
+            }`}
+          />
         </button>
         <div className="flex w-vw justify-between ">
-          <p className="text-cardPara">{title}</p>
+          <p
+            className={`${
+              !completed ? "text-cardPara" : `text-cardPara line-through`
+            }`}
+          >
+            {title}
+          </p>
           <button type="button">
             <FaTrash className="text-primary hover:animate-bounce" />
           </button>
