8e0a4158463aaf0a67abffc74a076ee5a669bf04
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-09-19 21:24:28 +0530

    added a bouncing line if the todo list is empty

--- client/src/pages/AllTodo.js
+++ client/src/pages/AllTodo.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
+
 import TodoItem from "../components/TodoItem";
 
 const AllTodo = () => {
   const { items } = useOutletContext();
+
+  if (items.length === 0) {
+    return (
+      <div className="w-3/4 h-2 bg-tertiary rounded-2xl mx-auto mt-5 animate-bounce"></div>
+    );
+  }
+
   return (
     <>
       {items.map((item) => {
--- client/src/pages/CompletedTodo.js
+++ client/src/pages/CompletedTodo.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
+
 import TodoItem from "../components/TodoItem";
 
 const CompletedTodo = () => {
   const { items } = useOutletContext();
   const completedItems = items.filter((item) => item.completed === true);
+
+  if (completedItems.length === 0) {
+    return (
+      <div className="w-3/4 h-2 bg-tertiary rounded-2xl mx-auto mt-5 animate-bounce"></div>
+    );
+  }
 
   return (
     <>
