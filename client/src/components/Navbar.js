13e73d7719ed5029fd50a6270beaef85985e3c3e
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-09-19 22:15:15 +0530

    replaced Links with NavLink

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,31 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = (props) => {
   const { todoCount } = props;
-  console.log(todoCount);
+
+  let activeClassName = "text-gray-300 scale-125 ";
 
   return (
     <>
       <div className="font-head text-subHeadline capitalize flex items-center justify-center gap-16 bg-secondary h-12 ">
-        <Link
+        <NavLink
           to="/"
-          className=" hover:text-gray-300 hover:scale-125 hover:transition-all  "
+          className={({ isActive }) =>
+            isActive
+              ? activeClassName
+              : "hover:text-gray-300 hover:scale-125 hover:transition-all"
+          }
         >
           todo({todoCount.homeCount})
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="/completed"
-          className=" hover:hover:text-gray-300  hover:scale-125 hover:transition-all "
+          className={({ isActive }) =>
+            isActive
+              ? activeClassName
+              : "hover:text-gray-300 hover:scale-125 hover:transition-all"
+          }
         >
           complted({todoCount.completedCount})
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="/all"
-          className="hover:text-gray-300 hover:scale-125 hover:transition-all "
+          className={({ isActive }) =>
+            isActive
+              ? activeClassName
+              : "hover:text-gray-300 hover:scale-125 hover:transition-all"
+          }
         >
           All({todoCount.totalCount})
-        </Link>
+        </NavLink>
       </div>
     </>
   );
