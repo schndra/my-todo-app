df3eec453b66db45f9c0fcdecf1757d672922016
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-10-05 19:49:06 +0530

    created Todo model

---
+++ server/models/Todo.js
@@ -0,0 +1,14 @@
+import mongoose, { mongo } from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model("Todo", todoSchema);
